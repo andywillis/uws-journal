b3f85c9a7f6c8e66fd241f340cceb68eae7512a3
Andy Willis <development@awillis.fastmail.fm>
2018-11-02 23:03:05 +0000

    Added dist for Heroku

--- src/server/auth/index.js
+++ src/server/auth/index.js
@@ -3,8 +3,8 @@
 const { google } = require('googleapis');
 
 function authorise({ credentials, token }, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials;
-  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+  const { client_secret, client_id, redirect_url } = credentials;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_url);
   oAuth2Client.setCredentials(token);
   callback(oAuth2Client);
 }
