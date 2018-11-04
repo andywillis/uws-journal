426bbf37053aef9b075bec82bc5276d5d3da3513
Andy Willis <andywillis@users.noreply.github.com>
2018-11-04 12:53:11 +0000

    Reverted to v22 of Jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testURL: 'http://localhost/',
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.(js|jsx)?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
+};
