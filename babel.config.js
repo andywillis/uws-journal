2daf6b3a348edc707a9e562c0c42c27a70aa6bf9
Andy Willis <andywillis@users.noreply.github.com>
2018-11-09 01:37:06 +0000

    Added classfields plugin (#15)

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,7 @@ module.exports = function babelConfig(api) {
     plugins: [
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-transform-regenerator',
+      '@babel/plugin-proposal-class-properties'
     ],
     env: {
       test: {
