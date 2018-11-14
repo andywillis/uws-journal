513315efe1a48aee01cfb2ee27225e98b6c32a91
Andy Willis <andywillis@users.noreply.github.com>
2018-11-14 21:48:59 +0000

    Uses bundle splitting (#16)

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,27 @@ module.exports = (env, argv) => ({
   output: {
     publicPath: '/',
     path: path.resolve(__dirname, 'dist'),
-    filename: argv.mode === 'development' ? 'bundle.js' : 'bundle.[hash].js',
+    filename: argv.mode === 'development' ? '[name].bundle.js' : '[name].bundle.[hash].js',
   },
   optimization: {
+  	splitChunks: {
+      chunks: 'all',
+      // maxInitialRequests: Infinity,
+      // minSize: 0,
+      // cacheGroups: {
+      //   vendor: {
+      //     test: /[\\/]node_modules[\\/]/,
+      //     name(module) {
+      //       // get the name. E.g. node_modules/packageName/not/this/part.js
+      //       // or node_modules/packageName
+      //       const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+      //       // npm package names are URL-safe, but some servers don't like @ symbols
+      //       return `npm.${packageName.replace('@', '')}`;
+      //     }
+      //   }
+      // }
+		},
     minimizer: [
       new UglifyJsPlugin({
         parallel: true,
