6be8a0054587807f46277a45c0b24fee2d7dd728
Andy Willis <andywillis@users.noreply.github.com>
2018-12-12 12:25:55 +0000

    New placeholdered images (#25)

--- src/client/components/Image/index.js
+++ src/client/components/Image/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { createElement, Component } from 'react';
+import compileClasses from 'classnames';
+
 import { getDeviceDimensions } from '../../lib/device';
 
 import style from './style.css';
@@ -11,22 +12,79 @@ function getResponsiveSrc(x, src) {
   return `${src}.jpg`;
 }
 
+function splitAltString(alt) {
+  return alt.match(/^(.+) (\d+)x(\d+)$/);
+}
 
-const Image = ({ src, alt }) => {
-  const { deviceWidth } = getDeviceDimensions();
-  return (
-    <img
-      className={style.image}
-      src={getResponsiveSrc(deviceWidth, src)}
-      alt={alt}
-    />
-  );
-};
+function getRatio(width, height) {
+  return width / height;
+}
+
+class Image extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isLoaded: false };
+    this.getImageElement = this.getImageElement.bind(this);
+    this.handleLoad = this.handleLoad.bind(this);
+    this.setNode = this.setNode.bind(this);
+  }
+
+  getImageElement({ className, width, height, altText, src }) {
+    return createElement('img', {
+      className,
+      width,
+      height,
+      alt: altText,
+      onLoad: this.handleLoad,
+      src
+    });
+  }
+
+  setNode(node) {
+    this.node = node;
+  }
+
+  getNewWidth() {
+    return Number(window.getComputedStyle(this.node).width.replace('px', ''));
+  }
+
+  handleLoad() {
+    this.setState(({ isLoaded }) => {
+      return { isLoaded: !isLoaded };
+    });
+  }
+
+  render() {
+
+    const { isLoaded } = this.state;
+    const { alt, src: url } = this.props;
+
+    const [ , altText, width, height ] = splitAltString(alt);
+    const { deviceWidth } = getDeviceDimensions();
+
+    const src = getResponsiveSrc(deviceWidth, url);
+    const className = compileClasses(style.image, isLoaded && style.show);
+
+    const aspectRatio = getRatio(width, height);
+
+    const containerHeight = isLoaded && this.node
+      ? this.getNewWidth() / aspectRatio
+      : height;
+
+    return (
+      <div
+        className={style.imageContainer}
+        style={{ width: '80%', height: `${containerHeight}px` }}
+        ref={this.setNode}
+      >
+        {this.getImageElement({ className, width, height, altText, src })}
+      </div>
+    );
+
+  }
+
+}
 
 
 export default Image;
-
-Image.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired
-};
