baafb73905640e09deb3ac9e6f73caaafeca3c64
Andy Willis <github@awillis.fastmail.fm>
2018-12-15 19:58:09 +0000

    Set up tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testURL: 'http://localhost/',
+  setupFiles:['./test/setupTests.js'],
   transform: {
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.(js|jsx)?$': 'babel-jest'
@@ -7,5 +8,6 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/']
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  snapshotSerializers: ['enzyme-to-json/serializer']
 };
--- src/client/components/Common/Heading/Heading.spec.js
+++ src/client/components/Common/Heading/Heading.spec.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
-import Heading from './index';
+import App from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <div>
-      <Heading
-        level="h2"
-        type="entry"
-        text="Test heading"
-        color="black"
-      />
-    </div>,
-    div
-  );
+describe('App component', () => {
+
+  const wrapper = shallow(<App />);
+
+  it('renders the component', () => {
+    expect(wrapper.length).not(undefined);
+  });
+
 });
