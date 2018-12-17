17a7d102441c63f08bc6365bda6ad5c86a90c0d4
Andy Willis <github@awillis.fastmail.fm>
2018-12-17 03:38:57 +0000

    Fixed CSS error (semi-colon outside of a rule)

--- src/client/components/Favourite/index.js
+++ src/client/components/Favourite/index.js
@@ -49,17 +49,16 @@ class Favourite extends Component {
 
   render() {
     const { status } = this.state;
+    const classNames = compileClasses(style.favourite, style[status]);
     return (
-      <div className={style.favourite}>
-        <span
-          tabIndex="0"
-          role="button"
-          className={style[status]}
-          onClick={this.handleFavourite}
-          onKeyUp={this.handleFavourite}
-        >&#x2605;
-        </span>
-      </div>
+      <button
+        className={classNames}
+        type="button"
+        tabIndex="0"
+        onClick={this.handleFavourite}
+        onKeyUp={this.handleFavourite}
+      >&#x2605;
+      </button>
     );
   }
 }
